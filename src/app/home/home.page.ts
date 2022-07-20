a78cbbe4a4e524af92c072e20ed038750c61ac73
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-07-20 23:34:12 +0200

    ONA-2: poc sticky side e overlay

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -20,13 +20,22 @@ export class HomePage implements AfterViewInit {
         var step = article.selectAll(".step");
 
         // initialize the scrollama
-        var scroller = scrollama();
+        var scroller = scrollama() as any;
+
+
+        var scrollyOverlay = main.select("#scrolly_overlay");
+        var figureOverlay = scrollyOverlay.select("figure");
+        var articleOverlay = scrollyOverlay.select("article");
+        var stepOverlay = articleOverlay.selectAll(".step");
+
+        var scrollerOverlay = scrollama() as any;
 
         // generic window resize listener event
         function handleResize() {
             // 1. update height of step elements
             var stepH = Math.floor(window.innerHeight * 0.75);
             step.style("height", stepH + "px");
+            stepOverlay.style("height", stepH + "px");
 
             var figureHeight = window.innerHeight / 2;
             var figureMarginTop = (window.innerHeight - figureHeight) / 2;
@@ -35,8 +44,13 @@ export class HomePage implements AfterViewInit {
                 .style("height", figureHeight + "px")
                 .style("top", figureMarginTop + "px");
 
+            figureOverlay
+                .style("height", figureHeight + "px")
+                .style("top", figureMarginTop + "px");
+
             // 3. tell scrollama to update new element dimensions
             scroller.resize();
+            scrollerOverlay.resize();
         }
 
         // scrollama event handlers
@@ -53,14 +67,22 @@ export class HomePage implements AfterViewInit {
             figure.select("p").text(response.index + 1);
         }
 
-        // function setupStickyfill() {
-        //     d3.selectAll(".sticky").each(function () {
-        //         Stickyfill.add(this);
-        //     });
-        // }
+        // scrollama event handlers
+        function handleStepEnterOverlay(response) {
+            console.log(response);
+            // response = { element, direction, index }
+
+            // add color to current step only
+            stepOverlay.classed("is-active", function (d, i) {
+                return i === response.index;
+            });
+
+            // update graphic based on step
+            figureOverlay.select("p").text(response.index + 1);
+        }
+
 
         function init() {
-            // setupStickyfill();
 
             // 1. force a resize on load to ensure proper dimensions are sent to scrollama
             handleResize();
@@ -69,62 +91,24 @@ export class HomePage implements AfterViewInit {
             // 		this will also initialize trigger observations
             // 3. bind scrollama event handlers (this can be chained like below)
             scroller
-                .setup(({
+                .setup({
                     step: "#scrolly article .step",
                     offset: 0.33,
                     debug: false
-                } as any))
-                .onStepEnter(handleStepEnter);
-        }
-
-        // kick things off
-        init();
-    }
-    __ngAfterViewInit() {
-        var scrolly = document.querySelector("#scrolly");
-        var article = scrolly.querySelector("article");
-        var step = article.querySelectorAll(".step");
-
-        // initialize the scrollama
-        var scroller = scrollama();
-
-        // scrollama event handlers
-        function handleStepEnter(response) {
-            // response = { element, direction, index }
-            console.log(response);
-            // add to color to current step
-            response.element.classList.add("is-active");
-        }
-
-        function handleStepExit(response) {
-            // response = { element, direction, index }
-            console.log(response);
-            // remove color from current step
-            response.element.classList.remove("is-active");
-        }
-
-        function init() {
-            // set random padding for different step heights (not required)
-            step.forEach(function (step) {
-                var v = 100 + Math.floor((Math.random() * window.innerHeight) / 4);
-                (step as any).style.padding = v + "px 0px";
-            });
-
-            // 1. setup the scroller with the bare-bones options
-            // 		this will also initialize trigger observations
-            // 2. bind scrollama event handlers (this can be chained like below)
-            scroller
-                .setup({
-                    step: "#scrolly article .step",
-                    debug: false,
-                    offset: 0.5
                 })
-                .onStepEnter(handleStepEnter)
-                .onStepExit(handleStepExit);
+                .onStepEnter(handleStepEnter);
 
+            scrollerOverlay
+                .setup({
+                    step: "#scrolly_overlay article .step",
+                    offset: 0.33,
+                    debug: false
+                })
+                .onStepEnter(handleStepEnterOverlay);
         }
 
         // kick things off
         init();
     }
+ 
 }
